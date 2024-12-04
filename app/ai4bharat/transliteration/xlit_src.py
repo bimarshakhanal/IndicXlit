63ca56401fef1b1224370ea41fa30458f5decf3d
Bimarsha Khanal <bimarsha@fusemachines.com>
2024-12-04 16:39:26 +0545

    clean code

--- app/ai4bharat/transliteration/xlit_src.py
+++ app/ai4bharat/transliteration/xlit_src.py
@@ -4,13 +4,11 @@ def XlitEngine(
     src_script_type = "roman",
 ):  
     print("LEVEL 1 reached", model_type)
-    print("SRC TYPE: ", src_script_type)
     if model_type == "transformer":
         if src_script_type in {"roman", "latin", "en"}:
             from .transformer import XlitEngineTransformer_En2Indic
             return XlitEngineTransformer_En2Indic(lang2use, beam_width=beam_width, rescore=rescore)
         elif src_script_type == "indic":
-            print("Source is indic")
             from .transformer import XlitEngineTransformer_Indic2En
             return XlitEngineTransformer_Indic2En(beam_width=beam_width, rescore=rescore)
     
